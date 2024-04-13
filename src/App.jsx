6aa01d7be18a28619d11ebf75cbbb4c07f1a17df
kasahun welela <getasewmequanint@cbe.com.et>
2024-04-13 12:54:10 +0300

    add practice area component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import Hero from "./components/Hero";
+import PracticeCard from "./components/PracticeCard";
 
 function App() {
   return (
     <>
       <Hero />
+      <PracticeCard />
     </>
   );
 }
