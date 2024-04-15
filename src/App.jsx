0bdbcea6b19e77cb69cae6e6d2d59146f848d977
kasahun welela <getasewmequanint@cbe.com.et>
2024-04-15 14:47:41 +0300

    create team component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Hero from "./components/Hero";
 import PracticeCard from "./components/PracticeCard";
 import NewsCard from "./components/NewsCard";
+import Teams from "./components/Teams";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <PracticeCard />
       <NewsCard />
+      <Teams />
     </>
   );
 }
