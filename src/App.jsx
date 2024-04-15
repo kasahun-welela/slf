22810fa26f5bf4aeb1fd133f292dffc719a07eb0
kasahun welela <getasewmequanint@cbe.com.et>
2024-04-15 10:58:07 +0300

    create news card

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import Hero from "./components/Hero";
 import PracticeCard from "./components/PracticeCard";
+import NewsCard from "./components/NewsCard";
 
 function App() {
   return (
     <>
       <Hero />
       <PracticeCard />
+      <NewsCard />
     </>
   );
 }
